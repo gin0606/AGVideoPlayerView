b9da8fdbf7e0a5f90df46bc0a1d6c6fd3fcf73a8
gin0606 <kkgn06@gmail.com>
2017-12-14 18:08:23 +0900

    Create new project

---
+++ AGVideoPlayerView/AGVideoPlayerView.h
@@ -0,0 +1,19 @@
+//
+//  AGVideoPlayerView.h
+//  AGVideoPlayerView
+//
+//  Created by gin0606 on 2017/12/14.
+//  Copyright © 2017年 gin0606. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AGVideoPlayerView.
+FOUNDATION_EXPORT double AGVideoPlayerViewVersionNumber;
+
+//! Project version string for AGVideoPlayerView.
+FOUNDATION_EXPORT const unsigned char AGVideoPlayerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AGVideoPlayerView/PublicHeader.h>
+
+
